10fdda7816bbd82729d59baa389e8b99bedc69a5
Shelby <MustangYM@yeah.net>
2023-05-23 16:45:09 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 int main(int argc, const char *argv[]) {
   do {
-    cout << termcolor::red << "Be a loser, be a happy soul." << generatorId << endl;
+    cout << "Be a loser, be a happy soul." << endl;
   } while (1);
   return 0;
 }
